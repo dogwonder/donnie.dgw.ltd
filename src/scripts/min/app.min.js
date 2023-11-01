59f56c8c444c13105dd79add7ce569036612215a
Rich Holman <richholman@dogwonderPro.local>
2023-11-01 17:29:36 +0000

    Text effects

---
+++ src/scripts/min/app.min.js
@@ -0,0 +1,55 @@
+;(function () {
+
+    'use strict';
+
+    const ruffleEmbeds = function() {
+
+        const ruffleSources = ['object.aspect-ratio ruffle-embed', 'object.aspect-ratio ruffle-player', '.ruffle-container'];
+
+        //If no ruffle embeds are found bail
+        if (!ruffleSources) return;
+
+        document.querySelectorAll(ruffleSources.join(",")).forEach((object) => {
+          object.style.maxWidth = "100%";
+          object.style.height = "auto";
+          object.style.aspectRatio = `${object.getAttribute(
+            "width"
+          )} / ${object.getAttribute("height")}`;
+        });
+
+    };
+
+    // const animateText = function( element, audioFile ) {
+
+    //     const text = document.getElementById(element);
+    //     //If no text is found bail
+    //     if (!text) return;
+
+    //     const words = text.innerHTML.split(' ');
+    //     text.innerHTML = '';
+
+    //     const numTimeouts = words.length;
+    //     let timeoutsFinished = 0;
+
+    //     const audio = new Audio(audioFile);
+
+    //     for (let i = 0; i < words.length; i++) {
+    //     setTimeout(() => {
+    //             text.innerHTML += words[i] + ' ';
+    //             timeoutsFinished++;
+    //             if (timeoutsFinished === numTimeouts) {
+    //                 audio.pause();
+    //                 const event = new CustomEvent('timeoutsCompleted');
+    //                 document.dispatchEvent(event);
+    //             }
+    //         }, i * 10);
+    //     }
+    // };
+
+    document.addEventListener("DOMContentLoaded", function() {
+
+        ruffleEmbeds();
+
+    }); 
+
+})();
