6f27e9b1720ab5f05a3a22eca44bd8966557a547
Rich Holman <richholman@dogwonderPro.local>
2023-11-02 17:13:36 +0000

    Move 98 back

--- .eleventy.js
+++ .eleventy.js
@@ -22,7 +22,6 @@ module.exports = eleventyConfig => {
     eleventyConfig.addPassthroughCopy({"src/fonts": "fonts"});
     eleventyConfig.addPassthroughCopy({"src/audio": "mp3"});
     eleventyConfig.addPassthroughCopy({"src/scss/vendor/98": "css"});
-    
 
     //Tailwind
     eleventyConfig.addWatchTarget('tailwind.config.js')
